4a3124d1d00ebce6c7f7aa1ff8f5b23cc8709658
Ezerhel <voronkovilya39@gmail.com>
2022-05-03 22:59:11 +0200

    Initial Commit

---
+++ Words Game/Words_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Words_GameApp.swift
+//  Words Game
+//
+//  Created by Admin on 03.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct Words_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
